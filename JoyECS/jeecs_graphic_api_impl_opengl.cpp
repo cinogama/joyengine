56bb817d23b661dc516a490843751033858cb92f
mr_cino <583903928@qq.com>
2022-03-09 14:10:59 +0800

    * 添加glew到3rd

--- JoyECS/jeecs_graphic_api_impl_opengl.cpp
+++ JoyECS/jeecs_graphic_api_impl_opengl.cpp
@@ -3,24 +3,34 @@
 #define JE_IMPL
 #include "jeecs.hpp"
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#ifdef _WIN32
+#pragma comment(lib,"OpenGL32.lib")
+#endif
+
 // Here is low-level-graphic-api impl.
 // OpenGL version.
 
 jegl_graphic_api::custom_interface_info_t gl_startup(jegl_thread*, const jegl_interface_config*)
 {
-    jeecs::debug::log("Graphic Inited!");
+    jeecs::debug::log("Graphic thread start!");
+
+    glewInit();
 
     return nullptr;
 }
 
 void gl_update(jegl_thread*, jegl_graphic_api::custom_interface_info_t)
 {
-    jeecs::debug::log("Graphic Update!");
+    jeecs::debug::log("Graphic thread update!");
 }
 
 void gl_shutdown(jegl_thread*, jegl_graphic_api::custom_interface_info_t)
 {
-    jeecs::debug::log("Graphic Shutdown!");
+    jeecs::debug::log("Graphic thread shutdown!");
+
 }
 
 JE_API void jegl_using_opengl_apis(jegl_graphic_api* write_to_apis)
