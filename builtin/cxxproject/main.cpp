27a4c28fc553d8b6c18302edb2bb475820672532
mr_cino <583903928@qq.com>
2022-10-05 23:09:46 +0800

    * 支持编译和加载模块中的系统和组件（TODO; 支持编辑器内添加组件和系统定义）

--- builtin/cxxproject/main.cpp
+++ builtin/cxxproject/main.cpp
@@ -1,15 +1,18 @@
 #include "jeecs.hpp"
+#include "loader.hpp"
 
 // Do not modify this file!
+extern "C" {
+	JE_EXPORT void jeecs_module_entry()
+	{
+		jeecs::enrty::module_entry();
+		jemodule::register_types();
+	}
 
-JE_EXPORT void jeecs_module_entry()
-{
-	jeecs::enrty::module_entry();
-}
-
-JE_EXPORT void jeecs_module_leave()
-{
-	jeecs::enrty::module_leave();
+	JE_EXPORT void jeecs_module_leave()
+	{
+		jeecs::enrty::module_leave();
+	}
 }
 
 static_assert(std::is_same<decltype(&jeecs_module_entry), 
