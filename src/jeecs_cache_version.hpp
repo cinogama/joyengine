f171d2bd95bbe0e5ff912f8a69d93c958c1348bb
mr_cino <mr_cino@cinogama.net>
2023-06-18 22:23:55 +0800

    * 创建项目时自带gitignore文件

--- src/jeecs_cache_version.hpp
+++ src/jeecs_cache_version.hpp
@@ -1,10 +1,4 @@
 #pragma once
-#ifndef JE_IMPL
-#   define JE_IMPL
-#   define JE_ENABLE_DEBUG_API
-#   include "jeecs.hpp"
-#endif
-
 constexpr uint32_t cache_mgr_version = 0x00000002;
 constexpr uint32_t IMAGE_CACHE_VERSION = 0x00000001;
-constexpr uint32_t SHADER_CACHE_VERSION = 0x0000010B;
+constexpr uint32_t SHADER_CACHE_VERSION = 0x0000010C;
