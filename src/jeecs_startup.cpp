b2dbc1527aad5bc131a156e438dba20aeb408291
mr_cino <mr_cino@cinogama.net>
2023-08-09 21:46:24 +0800

    * fix

--- src/jeecs_startup.cpp
+++ src/jeecs_startup.cpp
@@ -175,7 +175,7 @@ void je_finish()
 
 const char* je_build_version()
 {
-    return "JoyEngine 4.0.dev " __TIMESTAMP__;
+    return "JoyEngine 4.0.0 " __TIMESTAMP__;
 }
 
 const char* je_build_commit()
