4c13654d9834c85ac94eba52b93fbcd2dfd90464
mr_cino <mr_cino@cinogama.net>
2023-11-05 02:52:53 +0800

    * 修正C++模块的行为

--- builtin/cxxproject/main.cpp
+++ builtin/cxxproject/main.cpp
@@ -1,16 +1,20 @@
 #include "jeecs.hpp"
 #include "loader.hpp"
 
+jeecs::typing::type_unregister_guard* _module_guard;
+
 extern "C" {
 	JE_EXPORT void jeecs_module_entry()
 	{
-		jemodule::register_types();
-		jeecs::entry::module_entry();
+		_module_guard = new jeecs::typing::type_unregister_guard();
+		jemodule::register_types(_module_guard);
+		jeecs::entry::module_entry(_module_guard);
 	}
 
 	JE_EXPORT void jeecs_module_leave()
 	{
-		jeecs::entry::module_leave();
+		jeecs::entry::module_leave(_module_guard);
+		delete _module_guard;
 	}
 }
 
