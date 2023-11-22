34153137ed6d513ab7ed27306ff9762d8829621d
mr_cino <mr_cino@cinogama.net>
2023-11-22 15:12:57 +0800

    * Release 4.3.3

--- src/jeecs_startup.cpp
+++ src/jeecs_startup.cpp
@@ -389,7 +389,7 @@ void je_finish()
 
 const char* je_build_version()
 {
-    return "JoyEngine 4.3.2 " __TIMESTAMP__;
+    return "JoyEngine 4.3.3 " __TIMESTAMP__;
 }
 
 const char* je_build_commit()
