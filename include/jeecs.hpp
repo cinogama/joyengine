59a0363ba97c6a76e4af7272b53a8142c1118ce1
mr_cino <mr_cino@cinogama.net>
2024-01-24 00:56:03 +0800

    * RELEASE 4.4.7

--- include/jeecs.hpp
+++ include/jeecs.hpp
@@ -9,7 +9,7 @@
 #include "wo.h"
 
 #define JE_VERSION_WRAP(A, B, C) #A "." #B "." #C
-#define JE_CORE_VERSION JE_VERSION_WRAP(4, 4, 6)
+#define JE_CORE_VERSION JE_VERSION_WRAP(4, 4, 7)
 
 #include <cstdint>
 #include <cstring>
