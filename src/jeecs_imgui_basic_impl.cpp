05443a83f0e22b366c441379425411a8cf8140fe
mr_cino <mr_cino@cinogama.net>
2024-06-23 17:07:51 +0800

    * Update

--- src/jeecs_imgui_basic_impl.cpp
+++ src/jeecs_imgui_basic_impl.cpp
@@ -358,27 +358,27 @@ R"(
     public enum ImGuiInputTextFlags
     {
         ImGuiInputTextFlags_None                = 0,
-        ImGuiInputTextFlags_CharsDecimal        = 1 << 0,   // Allow 0123456789.+-*/
-        ImGuiInputTextFlags_CharsHexadecimal    = 1 << 1,   // Allow 0123456789ABCDEFabcdef
-        ImGuiInputTextFlags_CharsUppercase      = 1 << 2,   // Turn a..z into A..Z
-        ImGuiInputTextFlags_CharsNoBlank        = 1 << 3,   // Filter out spaces, tabs
-        ImGuiInputTextFlags_AutoSelectAll       = 1 << 4,   // Select entire text when first taking mouse focus
-        ImGuiInputTextFlags_EnterReturnsTrue    = 1 << 5,   // Return 'true' when Enter is pressed (as opposed to every time the value was modified). Consider looking at the IsItemDeactivatedAfterEdit() function.
-        ImGuiInputTextFlags_CallbackCompletion  = 1 << 6,   // Callback on pressing TAB (for completion handling)
-        ImGuiInputTextFlags_CallbackHistory     = 1 << 7,   // Callback on pressing Up/Down arrows (for history handling)
-        ImGuiInputTextFlags_CallbackAlways      = 1 << 8,   // Callback on each iteration. User code may query cursor position, modify text buffer.
-        ImGuiInputTextFlags_CallbackCharFilter  = 1 << 9,   // Callback on character inputs to replace or discard them. Modify 'EventChar' to replace or discard, or return 1 in callback to discard.
-        ImGuiInputTextFlags_AllowTabInput       = 1 << 10,  // Pressing TAB input a '\t' character into the text field
-        ImGuiInputTextFlags_CtrlEnterForNewLine = 1 << 11,  // In multi-line mode, unfocus with Enter, add new line with Ctrl+Enter (default is opposite: unfocus with Ctrl+Enter, add line with Enter).
-        ImGuiInputTextFlags_NoHorizontalScroll  = 1 << 12,  // Disable following the cursor horizontally
-        ImGuiInputTextFlags_AlwaysOverwrite     = 1 << 13,  // Overwrite mode
-        ImGuiInputTextFlags_ReadOnly            = 1 << 14,  // Read-only mode
-        ImGuiInputTextFlags_Password            = 1 << 15,  // Password mode, display all characters as '*'
-        ImGuiInputTextFlags_NoUndoRedo          = 1 << 16,  // Disable undo/redo. Note that input text owns the text data while active, if you want to provide your own undo/redo stack you need e.g. to call ClearActiveID().
-        ImGuiInputTextFlags_CharsScientific     = 1 << 17,  // Allow 0123456789.+-*/eE (Scientific notation input)
-        ImGuiInputTextFlags_CallbackResize      = 1 << 18,  // Callback on buffer capacity changes request (beyond 'buf_size' parameter value), allowing the string to grow. Notify when the string wants to be resized (for string types which hold a cache of their Size). You will be provided a new BufSize in the callback and NEED to honor it. (see misc/cpp/imgui_stdlib.h for an example of using this)
-        ImGuiInputTextFlags_CallbackEdit        = 1 << 19,  // Callback on any edit (note that InputText() already returns true on edit, the callback is useful mainly to manipulate the underlying buffer while focus is active)
-        ImGuiInputTextFlags_EscapeClearsAll     = 1 << 20,  // Escape key clears content if not empty, and deactivate otherwise (contrast to default behavior of Escape to revert)
+        ImGuiInputTextFlags_CharsDecimal        = 0x00000001,   // Allow 0123456789.+-*/
+        ImGuiInputTextFlags_CharsHexadecimal    = 0x00000002,   // Allow 0123456789ABCDEFabcdef
+        ImGuiInputTextFlags_CharsUppercase      = 0x00000004,   // Turn a..z into A..Z
+        ImGuiInputTextFlags_CharsNoBlank        = 0x00000008,   // Filter out spaces, tabs
+        ImGuiInputTextFlags_AutoSelectAll       = 0x00000010,   // Select entire text when first taking mouse focus
+        ImGuiInputTextFlags_EnterReturnsTrue    = 0x00000020,   // Return 'true' when Enter is pressed (as opposed to every time the value was modified). Consider looking at the IsItemDeactivatedAfterEdit() function.
+        ImGuiInputTextFlags_CallbackCompletion  = 0x00000040,   // Callback on pressing TAB (for completion handling)
+        ImGuiInputTextFlags_CallbackHistory     = 0x00000080,   // Callback on pressing Up/Down arrows (for history handling)
+        ImGuiInputTextFlags_CallbackAlways      = 0x00000100,   // Callback on each iteration. User code may query cursor position, modify text buffer.
+        ImGuiInputTextFlags_CallbackCharFilter  = 0x00000200,   // Callback on character inputs to replace or discard them. Modify 'EventChar' to replace or discard, or return 1 in callback to discard.
+        ImGuiInputTextFlags_AllowTabInput       = 0x00000400,  // Pressing TAB input a '\t' character into the text field
+        ImGuiInputTextFlags_CtrlEnterForNewLine = 0x00000800,  // In multi-line mode, unfocus with Enter, add new line with Ctrl+Enter (default is opposite: unfocus with Ctrl+Enter, add line with Enter).
+        ImGuiInputTextFlags_NoHorizontalScroll  = 0x00001000,  // Disable following the cursor horizontally
+        ImGuiInputTextFlags_AlwaysOverwrite     = 0x00002000,  // Overwrite mode
+        ImGuiInputTextFlags_ReadOnly            = 0x00004000,  // Read-only mode
+        ImGuiInputTextFlags_Password            = 0x00008000,  // Password mode, display all characters as '*'
+        ImGuiInputTextFlags_NoUndoRedo          = 0x00010000,  // Disable undo/redo. Note that input text owns the text data while active, if you want to provide your own undo/redo stack you need e.g. to call ClearActiveID().
+        ImGuiInputTextFlags_CharsScientific     = 0x00020000,  // Allow 0123456789.+-*/eE (Scientific notation input)
+        ImGuiInputTextFlags_CallbackResize      = 0x00040000,  // Callback on buffer capacity changes request (beyond 'buf_size' parameter value), allowing the string to grow. Notify when the string wants to be resized (for string types which hold a cache of their Size). You will be provided a new BufSize in the callback and NEED to honor it. (see misc/cpp/imgui_stdlib.h for an example of using this)
+        ImGuiInputTextFlags_CallbackEdit        = 0x00080000,  // Callback on any edit (note that InputText() already returns true on edit, the callback is useful mainly to manipulate the underlying buffer while focus is active)
+        ImGuiInputTextFlags_EscapeClearsAll     = 0x00100000,  // Escape key clears content if not empty, and deactivate otherwise (contrast to default behavior of Escape to revert)
 
         // Obsolete names
         //ImGuiInputTextFlags_AlwaysInsertMode  = ImGuiInputTextFlags_AlwaysOverwrite   // [renamed in 1.82] name was not matching behavior
