9fbe0118251f9547fd916a1ed8226bb37ed8934c
mr_cino <mr_cino@cinogama.net>
2025-06-13 00:29:51 +0800

    * Update woolang 1.14.7.1

--- 拉取最新的包.bat
+++ 拉取最新的包.bat
@@ -6,6 +6,6 @@ cd 3rd/woolang
 git checkout release
 git pull
 cd ../..
-call baozi install -nonative -source
-call baozi install -nonative -profile debug
+call baozi install -nonative -source || (pause & exit /b 1)
+call baozi install -nonative -profile debug || (pause & exit /b 1)
 pause
