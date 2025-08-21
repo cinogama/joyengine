23ae4eef01126a37a846ce3c94d04ea85f8ccdfa
mr_cino <mr_cino@cinogama.net>
2025-08-21 20:51:55 +0800

    * 添加 vk glfw 初始化失败的情况下提供的信息

--- src/jeecs_graphic_api_impl_vk130.cpp
+++ src/jeecs_graphic_api_impl_vk130.cpp
@@ -1760,7 +1760,11 @@ namespace jeecs::graphic::api::vk130
             if (VK_SUCCESS != glfwCreateWindowSurface(
                 _vk_instance, (GLFWwindow*)_vk_jegl_interface->interface_handle(), nullptr, &_vk_surface))
             {
-                jeecs::debug::logfatal("Failed to create vk130 glfw surface.");
+                char* error_message = nullptr;
+                auto eno = glfwGetError(&error_message);
+
+                jeecs::debug::logfatal("Failed to create vk130 glfw surface: %s(%d).",
+                    error_message, eno);
             }
 #endif
             // 获取可接受的设备
