9015c5ab7b99b8669fe85201c2c6d9ef4b547495
mr_cino <mr_cino@cinogama.net>
2025-09-02 23:07:56 +0800

    * fix

--- src/jeecs_graphic_api_impl_metal.cpp
+++ src/jeecs_graphic_api_impl_metal.cpp
@@ -18,177 +18,15 @@ namespace jeecs::graphic::api::metal
     {
         JECS_DISABLE_MOVE_AND_COPY(jegl_metal_context);
 
-        struct user_interface_context_t
-        {
-            NS::Window* m_window;
-
-            MTL::Device* m_metal_device;
-            MTK::View* m_metal_view;
-        };
-
         jegl_metal_context() = default;
         ~jegl_metal_context() = default;
 
-        // m_user_interface_context will be init by `applicationDidFinishLaunching`
-        user_interface_context_t* m_user_interface_context;
-    };
-
-    class application_delegate
-        : public NS::ApplicationDelegate
-        , public MTK::ViewDelegate
-    {
-        static NS::Menu* createMenuBar()
-        {
-            using NS::StringEncoding::UTF8StringEncoding;
-
-            NS::Menu* pMainMenu = NS::Menu::alloc()->init();
-            NS::MenuItem* pAppMenuItem = NS::MenuItem::alloc()->init();
-            NS::Menu* pAppMenu = NS::Menu::alloc()->init(NS::String::string("Appname", UTF8StringEncoding));
-
-            NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
-            NS::String* quitItemName = NS::String::string("Quit ", UTF8StringEncoding)->stringByAppendingString(appName);
-            SEL quitCb = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* pSender) {
-                auto pApp = NS::Application::sharedApplication();
-                pApp->terminate(pSender);
-                });
-
-            NS::MenuItem* pAppQuitItem = pAppMenu->addItem(quitItemName, quitCb, NS::String::string("q", UTF8StringEncoding));
-            pAppQuitItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
-            pAppMenuItem->setSubmenu(pAppMenu);
-
-            NS::MenuItem* pWindowMenuItem = NS::MenuItem::alloc()->init();
-            NS::Menu* pWindowMenu = NS::Menu::alloc()->init(NS::String::string("Window", UTF8StringEncoding));
-
-            SEL closeWindowCb = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
-                auto pApp = NS::Application::sharedApplication();
-                pApp->windows()->object< NS::Window >(0)->close();
-                });
-            NS::MenuItem* pCloseWindowItem = pWindowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCb, NS::String::string("w", UTF8StringEncoding));
-            pCloseWindowItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
-
-            pWindowMenuItem->setSubmenu(pWindowMenu);
-
-            pMainMenu->addItem(pAppMenuItem);
-            pMainMenu->addItem(pWindowMenuItem);
-
-            pAppMenuItem->release();
-            pWindowMenuItem->release();
-            pAppMenu->release();
-            pWindowMenu->release();
-
-            return pMainMenu->autorelease();
-        }
-        graphic_syncer_host* m_engine_graphic_host;
-    private:
-        bool m_graphic_request_to_close;
-        std::optional<jegl_metal_context::user_interface_context_t> m_ui_context;
-    public:
-        application_delegate(graphic_syncer_host* ready_graphic_host)
-            : NS::ApplicationDelegate()
-            , MTK::ViewDelegate()
-            , m_engine_graphic_host(ready_graphic_host)
-            , m_graphic_request_to_close(false)
-            , m_ui_context(std::nullopt)
-        {
-        }
-        ~application_delegate()
-        {
-            if (m_ui_context.has_value())
-            {
-                auto& context = m_ui_context.value();
-
-                context.m_metal_view->release();
-                context.m_metal_device->release();
-                context.m_window->release();
-            }
-        }
-    public:
-        virtual void drawInMTKView(MTK::View* pView) override
-        {
-            if (m_graphic_request_to_close)
-                // Graphic has been requested to close.
-                return;
-
-            if (!m_engine_graphic_host->frame())
-            {
-                // TODO: Graphic requested to close, close windows and exit app.
-                m_graphic_request_to_close = true;
-            }
-        }
-
-        virtual void applicationWillFinishLaunching(
-            NS::Notification* pNotification) override
-        {
-            NS::Application* application =
-                reinterpret_cast<NS::Application*>(pNotification->object());
-
-            application->setMainMenu(createMenuBar());
-            application->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
-        }
-        virtual void applicationDidFinishLaunching(
-            NS::Notification* pNotification) override
-        {
-            NS::Application* application =
-                reinterpret_cast<NS::Application*>(pNotification->object());
-
-            assert(m_engine_graphic_host != nullptr);
-            auto& metal_ui_context = m_ui_context.emplace();
-
-            jegl_context* engine_raw_graphic_context =
-                m_engine_graphic_host->get_graphic_context_after_context_ready();
-
-            jegl_metal_context* engine_graphic_context =
-                reinterpret_cast<jegl_metal_context*>(
-                    engine_raw_graphic_context->m_graphic_impl_context);
-
-            const jegl_interface_config& engine_raw_graphic_config =
-                engine_raw_graphic_context->m_config;
-
-            // Init m_user_interface_context.
-            engine_graphic_context->m_user_interface_context = &metal_ui_context;
-
-            CGRect frame = (CGRect){
-                {100.0, 100.0},
-                {
-                    (double)engine_raw_graphic_config.m_width,
-                    (double)engine_raw_graphic_config.m_height} };
-
-            metal_ui_context.m_window = NS::Window::alloc()->init(
-                frame,
-                NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled,
-                NS::BackingStoreBuffered,
-                false);
-
-            metal_ui_context.m_metal_device = MTL::CreateSystemDefaultDevice();
-            assert(metal_ui_context.m_metal_device != nullptr);
-
-            metal_ui_context.m_metal_view =
-                MTK::View::alloc()->init(frame, metal_ui_context.m_metal_device);
-            metal_ui_context.m_metal_view->setColorPixelFormat(
-                MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
-            metal_ui_context.m_metal_view->setClearColor(
-                MTL::ClearColor::Make(1.0, 0.0, 0.0, 1.0));
-
-            metal_ui_context.m_metal_view->setDelegate(this);
-            metal_ui_context.m_window->setContentView(metal_ui_context.m_metal_view);
-
-            metal_ui_context.m_window->setTitle(
-                NS::String::string(
-                    engine_raw_graphic_config.m_title,
-                    NS::StringEncoding::UTF8StringEncoding));
-            metal_ui_context.m_window->makeKeyAndOrderFront(nullptr);
-
-            application->activateIgnoringOtherApps(true);
-        }
-        virtual bool applicationShouldTerminateAfterLastWindowClosed(
-            NS::Application* pSender) override
-        {
-            return true;
-        }
+        std::unique_ptr<jeecs::graphic::metal::window_and_device>
+            m_window_and_device;
     };
 
     jegl_context::graphic_impl_context_t
-        startup(jegl_context* glthread, const jegl_interface_config*, bool reboot)
+        startup(jegl_context* glthread, const jegl_interface_config* cfg, bool reboot)
     {
         if (!reboot)
             jeecs::debug::log("Graphic thread (Metal) start!");
@@ -202,6 +40,16 @@ namespace jeecs::graphic::api::metal
             [](jegl_context*, jegl_resource*) {});*/
 
         jegl_metal_context* context = new jegl_metal_context();
+
+        context->m_window_and_device =
+            std::make_unique<jeecs::graphic::metal::window_and_device>(
+                cfg->m_title,
+                (double)cfg->m_width,
+                (double)cfg->m_height);
+
+        const_cast<jegl_interface_config*>(cfg)->m_userdata =
+            context->m_window_and_device.get();
+
         return context;
     }
     void pre_shutdown(jegl_context*, jegl_context::graphic_impl_context_t, bool)
@@ -333,7 +181,7 @@ public:
             NS::AutoreleasePool* auto_release_pool =
                 NS::AutoreleasePool::alloc()->init();
 
-            jeecs::graphic::api::metal::application_delegate del(m_graphic_host);
+            jeecs::graphic::metal::application_delegate del(m_graphic_host);
 
             NS::Application* shared_application = NS::Application::sharedApplication();
             shared_application->setDelegate(&del);
@@ -351,7 +199,7 @@ void jegl_cocoa_metal_application_run(int argc, char** argv)
 }
 
 #else
-void jegl_using_metal_apis(jegl_graphic_api *write_to_apis)
+void jegl_using_metal_apis(jegl_graphic_api* write_to_apis)
 {
     jeecs::debug::logfatal("METAL not available.");
 }
--- src/jeecs_graphic_api_interface_cocoa.hpp
+++ src/jeecs_graphic_api_interface_cocoa.hpp
@@ -8,5 +8,155 @@
 
 namespace jeecs::graphic::metal
 {
+    struct window_and_device
+    {
+        NS::Window* m_window;
 
+        MTL::Device* m_metal_device;
+        MTK::View* m_metal_view;
+
+        window_and_device(const char* title, double width, double height)
+        {
+            CGRect frame = (CGRect){ {100.0, 100.0}, {width, height} };
+
+            m_window = NS::Window::alloc()->init(
+                frame,
+                NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled,
+                NS::BackingStoreBuffered,
+                false);
+
+            m_metal_device = MTL::CreateSystemDefaultDevice();
+            assert(m_metal_device != nullptr);
+
+            m_metal_view =
+                MTK::View::alloc()->init(frame, m_metal_device);
+            m_metal_view->setColorPixelFormat(
+                MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
+            m_metal_view->setClearColor(
+                MTL::ClearColor::Make(1.0, 0.0, 0.0, 1.0));
+
+            m_window->setContentView(m_metal_view);
+
+            m_window->setTitle(
+                NS::String::string(
+                    title, NS::StringEncoding::UTF8StringEncoding));
+            m_window->makeKeyAndOrderFront(nullptr);
+        }
+    };
+
+    class application_delegate
+        : public NS::ApplicationDelegate
+        , public MTK::ViewDelegate
+    {
+        static NS::Menu* createMenuBar()
+        {
+            using NS::StringEncoding::UTF8StringEncoding;
+
+            NS::Menu* pMainMenu = NS::Menu::alloc()->init();
+            NS::MenuItem* pAppMenuItem = NS::MenuItem::alloc()->init();
+            NS::Menu* pAppMenu = NS::Menu::alloc()->init(NS::String::string("Appname", UTF8StringEncoding));
+
+            NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
+            NS::String* quitItemName = NS::String::string("Quit ", UTF8StringEncoding)->stringByAppendingString(appName);
+            SEL quitCb = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* pSender) {
+                auto pApp = NS::Application::sharedApplication();
+                pApp->terminate(pSender);
+                });
+
+            NS::MenuItem* pAppQuitItem = pAppMenu->addItem(quitItemName, quitCb, NS::String::string("q", UTF8StringEncoding));
+            pAppQuitItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
+            pAppMenuItem->setSubmenu(pAppMenu);
+
+            NS::MenuItem* pWindowMenuItem = NS::MenuItem::alloc()->init();
+            NS::Menu* pWindowMenu = NS::Menu::alloc()->init(NS::String::string("Window", UTF8StringEncoding));
+
+            SEL closeWindowCb = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
+                auto pApp = NS::Application::sharedApplication();
+                pApp->windows()->object< NS::Window >(0)->close();
+                });
+            NS::MenuItem* pCloseWindowItem = pWindowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCb, NS::String::string("w", UTF8StringEncoding));
+            pCloseWindowItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
+
+            pWindowMenuItem->setSubmenu(pWindowMenu);
+
+            pMainMenu->addItem(pAppMenuItem);
+            pMainMenu->addItem(pWindowMenuItem);
+
+            pAppMenuItem->release();
+            pWindowMenuItem->release();
+            pAppMenu->release();
+            pWindowMenu->release();
+
+            return pMainMenu->autorelease();
+        }
+        graphic_syncer_host* m_engine_graphic_host;
+    public:
+        struct user_interface_context_t
+        {
+            NS::Window* m_window;
+
+            MTL::Device* m_metal_device;
+            MTK::View* m_metal_view;
+        };
+    private:
+        bool m_graphic_request_to_close;
+    public:
+        application_delegate(graphic_syncer_host* ready_graphic_host)
+            : NS::ApplicationDelegate()
+            , MTK::ViewDelegate()
+            , m_engine_graphic_host(ready_graphic_host)
+            , m_graphic_request_to_close(false)
+        {
+        }
+        ~application_delegate()
+        {
+        }
+    public:
+        virtual void drawInMTKView(MTK::View* pView) override
+        {
+            if (m_graphic_request_to_close)
+                // Graphic has been requested to close.
+                return;
+
+            if (!m_engine_graphic_host->frame())
+            {
+                // TODO: Graphic requested to close, close windows and exit app.
+                m_graphic_request_to_close = true;
+            }
+        }
+
+        virtual void applicationWillFinishLaunching(
+            NS::Notification* pNotification) override
+        {
+            NS::Application* application =
+                reinterpret_cast<NS::Application*>(pNotification->object());
+
+            application->setMainMenu(createMenuBar());
+            application->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
+        }
+        virtual void applicationDidFinishLaunching(
+            NS::Notification* pNotification) override
+        {
+            NS::Application* application =
+                reinterpret_cast<NS::Application*>(pNotification->object());
+
+            assert(m_engine_graphic_host != nullptr);
+
+            jegl_context* engine_raw_graphic_context =
+                m_engine_graphic_host->get_graphic_context_after_context_ready();
+            auto& config = engine_raw_graphic_context->m_config;
+
+            window_and_device* window_and_device_instance =
+                reinterpret_cast<window_and_device*>(config.m_userdata);
+
+            window_and_device_instance->m_metal_view->setDelegate(this);
+
+            application->activateIgnoringOtherApps(true);
+        }
+        virtual bool applicationShouldTerminateAfterLastWindowClosed(
+            NS::Application* pSender) override
+        {
+            return true;
+        }
+    };
 }
