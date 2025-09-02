188658bc781263eb545418dee9d21b44b5dcccb9
mr_cino <mr_cino@cinogama.net>
2025-09-02 17:55:58 +0800

    * TMP: 重构 v2

--- src/jeecs_graphic_api_interface_cocoa.hpp
+++ src/jeecs_graphic_api_interface_cocoa.hpp
@@ -6,11 +6,9 @@
 #include <AppKit/AppKit.hpp>
 #include <MetalKit/MetalKit.hpp>
 
-#include <optional>
-
 namespace jeecs::graphic::metal
 {
-    class application_delegate 
+    class application_delegate
         : public NS::ApplicationDelegate
         , public NS::ViewDelegate
     {
@@ -60,15 +58,20 @@ namespace jeecs::graphic::metal
         struct user_interface_context_t
         {
             NS::Window* m_window;
+
             MTL::Device* m_metal_device;
+            MTK::View* m_metal_view;
         };
     private:
+        bool m_graphic_request_to_close;
         std::optional<user_interface_context_t> m_ui_context;
     public:
         application_delegate(graphic_syncer_host* ready_graphic_host)
             : NS::ApplicationDelegate()
             , NS::ViewDelegate()
             , m_engine_graphic_host(ready_graphic_host)
+            , m_graphic_request_to_close(false)
+            , m_ui_context(std::nullopt)
         {
         }
         ~application_delegate()
@@ -76,9 +79,26 @@ namespace jeecs::graphic::metal
             if (m_ui_context.has_value())
             {
                 auto& context = m_ui_context.value();
+
+                context.m_metal_view->release();
+                context.m_metal_device->release();
+                context.m_window->release();
             }
         }
     public:
+        virtual void drawInMTKView(MTK::View* pView) override
+        {
+            if (m_graphic_request_to_close)
+                // Graphic has been requested to close.
+                return;
+
+            if (!m_engine_graphic_host->frame())
+            {
+                // TODO: Graphic requested to close, close windows and exit app.
+                m_graphic_request_to_close = true;
+            }
+        }
+
         virtual void applicationWillFinishLaunching(
             NS::Notification* pNotification) override
         {
@@ -91,11 +111,11 @@ namespace jeecs::graphic::metal
         virtual void applicationDidFinishLaunching(
             NS::Notification* pNotification) override
         {
-            NS::Application* application = 
+            NS::Application* application =
                 reinterpret_cast<NS::Application*>(pNotification->object());
 
             assert(m_engine_graphic_host != nullptr);
-            auto& context = m_ui_context.emplace();
+            auto& metal_ui_context = m_ui_context.emplace();
 
             jegl_context* engine_raw_graphic_context =
                 m_engine_graphic_host->get_graphic_context_after_context_ready();
@@ -103,8 +123,38 @@ namespace jeecs::graphic::metal
             jegl_interface_config& engine_raw_graphic_config =
                 engine_raw_graphic_context->m_config;
 
+            CGRect frame = (CGRect){
+                {100.0, 100.0},
+                {
+                    (double)engine_raw_graphic_config.m_width,
+                    (double)engine_raw_graphic_config.m_height} };
 
-            context.
+            metal_ui_context.m_window = NS::Window::alloc()->init(
+                frame,
+                NS::WindowStyleMaskClosable | NS::WindowStyleMaskTitled,
+                NS::BackingStoreBuffered,
+                false);
+
+            metal_ui_context.m_metal_device = MTL::CreateSystemDefaultDevice();
+            assert(metal_ui_context.m_metal_device != nullptr);
+
+            metal_ui_context.m_metal_view =
+                MTK::View::alloc()->init(frame, metal_ui_context.m_metal_device);
+            metal_ui_context.m_metal_view->setColorPixelFormat(
+                MTL::PixelFormat::PixelFormatBGRA8Unorm_sRGB);
+            metal_ui_context.m_metal_view->setClearColor(
+                MTL::ClearColor::Make(1.0, 0.0, 0.0, 1.0));
+
+            metal_ui_context.m_metal_view->setDelegate(this);
+            metal_ui_context.m_window->setContentView(metal_ui_context.m_metal_view);
+
+            metal_ui_context.m_window->setTitle(
+                NS::String::string(
+                    engine_raw_graphic_config.m_title,
+                    NS::StringEncoding::UTF8StringEncoding));
+            metal_ui_context.m_window->makeKeyAndOrderFront(nullptr);
+
+            application->activateIgnoringOtherApps(true);
         }
         virtual bool applicationShouldTerminateAfterLastWindowClosed(
             NS::Application* pSender) override
