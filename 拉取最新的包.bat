f18d3f3c95b6d3beff419d570ce1cbd7b3b05bd8
mr_cino <mr_cino@cinogama.net>
2025-10-24 16:55:51 +0800

    * 支持新版本 baozi 的版本锁

--- 拉取最新的包.bat
+++ 拉取最新的包.bat
@@ -6,6 +6,5 @@ cd 3rd/woolang
 git checkout release
 git pull
 cd ../..
-call baozi install -nonative -source || (pause & exit /b 1)
-call baozi install -nonative -profile debug || (pause & exit /b 1)
+call baozi install
 pause
